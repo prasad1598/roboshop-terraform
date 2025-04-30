f87104d688d7a271e853fb94ba1825cb0cccc36b
muniprasad <muniprasaddiddi@gmail.com>
2025-04-30 18:09:05 +0530

    files updated

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "databases" {
   ip_configuration_subnet_id = var.ip_configuration_subnet_id
   location                   = var.location
   network_security_group_id  = var.network_security_group_id
-  name                       = "frontend"
+  name                       = each.key
   rg_name                    = var.rg_name
   storage_image_reference_id = var.storage_image_reference_id
   zone_name                  = var.zone_name
@@ -17,7 +17,7 @@ module "application" {
   ip_configuration_subnet_id = var.ip_configuration_subnet_id
   location                   = var.location
   network_security_group_id  = var.network_security_group_id
-  name                       = "mongodb"
+  name                       = each.key
   rg_name                    = var.rg_name
   storage_image_reference_id = var.storage_image_reference_id
   zone_name                  = var.zone_name
