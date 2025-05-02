d4a717eb4f94fbc4aad097f5a9520991361cad4d
muniprasad <muniprasaddiddi@gmail.com>
2025-05-02 17:48:14 +0530

    files updated

--- environments/dev/dev_backend.tf
+++ environments/dev/dev_backend.tf
@@ -1,11 +1,10 @@
 terraform {
   backend "azurerm" {
     use_cli              = true
-    tenant_id            = "00000000-0000-0000-0000-000000000000"
     subscription_id      = "aa32da49-0603-4855-b55b-bfd4bcf7b16f"
     resource_group_name  = var.name_rg
-    storage_account_name = var.storage_account
-    container_name       = var.storage_container_name
+    storage_account_name = "terratfstateform"
+    container_name       = "roboshop-tfstate-files"
     key                  = "dev.terraform.tfstate"
   }
 }
--- environments/dev/variable.tf
+++ environments/dev/variable.tf
@@ -1,6 +1,3 @@
-variable "storage_account" {}
-variable "storage_container_name" {}
-
 variable "name_rg" {
   default = var.name_rg
 }
