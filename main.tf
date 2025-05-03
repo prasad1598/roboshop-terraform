ee9d8d7013294da55140e3687a76d6d42af4965a
muniprasad <muniprasaddiddi@gmail.com>
2025-05-03 17:59:03 +0530

    files updated

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "resource-group" {
   for_each = var.rg_name
   source   = "./modules/resource-group"
   name     = "${each.key}-${var.env}"
-  location = "each.value[location]"
+  location = each.value["location"]
 }
 
 module "databases" {
