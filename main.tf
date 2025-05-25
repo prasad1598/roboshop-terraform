e1bd4e874e22748377b856ca4953a23b9b636e0e
Muniprasad <muniprasaddiddi@gmail.com>
2025-05-25 15:50:46 +0530

    roboshop project update

--- main.tf
+++ main.tf
@@ -5,36 +5,36 @@ module "resource-group" {
   location = each.value["location"]
 }
 
-module "databases" {
-  for_each                   = var.databases
-  source                     = "./modules/vm"
-  name                       = each.key
-  rg_location                = module.resource-group[each.value["rgname"]].location
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  network_security_group_id  = var.network_security_group_id
-  rg_name                    = module.resource-group[each.value["rgname"]].name
-  storage_image_reference_id = var.storage_image_reference_id
-  zone_name                  = var.zone_name
-  dns_record_rg_name         = var.dns_record_rg_name
-  token                      = var.token
-  type                       = "db"
-}
-
-module "application" {
-  depends_on                 = [module.databases]
-  for_each                   = var.applications
-  source                     = "./modules/vm"
-  name                       = each.key
-  rg_location                = module.resource-group[each.value["rgname"]].location
-  network_security_group_id  = var.network_security_group_id
-  ip_configuration_subnet_id = var.ip_configuration_subnet_id
-  rg_name                    = module.resource-group[each.value["rgname"]].name
-  storage_image_reference_id = var.storage_image_reference_id
-  zone_name                  = var.zone_name
-  dns_record_rg_name         = var.dns_record_rg_name
-  token                      = var.token
-  type                       = "app"
-}
+# module "databases" {
+#   for_each                   = var.databases
+#   source                     = "./modules/vm"
+#   name                       = each.key
+#   rg_location                = module.resource-group[each.value["rgname"]].location
+#   ip_configuration_subnet_id = var.ip_configuration_subnet_id
+#   network_security_group_id  = var.network_security_group_id
+#   rg_name                    = module.resource-group[each.value["rgname"]].name
+#   storage_image_reference_id = var.storage_image_reference_id
+#   zone_name                  = var.zone_name
+#   dns_record_rg_name         = var.dns_record_rg_name
+#   token                      = var.token
+#   type                       = "db"
+# }
+#
+# module "application" {
+#   depends_on                 = [module.databases]
+#   for_each                   = var.applications
+#   source                     = "./modules/vm"
+#   name                       = each.key
+#   rg_location                = module.resource-group[each.value["rgname"]].location
+#   network_security_group_id  = var.network_security_group_id
+#   ip_configuration_subnet_id = var.ip_configuration_subnet_id
+#   rg_name                    = module.resource-group[each.value["rgname"]].name
+#   storage_image_reference_id = var.storage_image_reference_id
+#   zone_name                  = var.zone_name
+#   dns_record_rg_name         = var.dns_record_rg_name
+#   token                      = var.token
+#   type                       = "app"
+# }
 
 module "aks" {
   for_each    = var.aks
